67dea4e527bd6fe354a1605121593bdd18520683
Gundu Rakesh <113242509+GunduRakesh@users.noreply.github.com>
2023-02-14 16:52:24 +0530

    Update ec2-creation.tf

--- terraformscripts/ec2-creation.tf
+++ terraformscripts/ec2-creation.tf
@@ -5,7 +5,7 @@ resource "aws_instance" "AwsInstance"{
 	ami = "ami-0aa7d40eeae50c9a9"
 	instance_type = "t2.micro"
 	key_name = "Myserver1"
-	security_groups = ["launch-wizard-2"]
+	security_groups = ["launch-wizard-44"]
 	tags = {
 	  Name = "TomcatServer2"
     }
