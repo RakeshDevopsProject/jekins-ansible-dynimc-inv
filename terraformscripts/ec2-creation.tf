b99480087f318b2b92f8bc84737b9e455be2d10b
Gundu Rakesh <113242509+GunduRakesh@users.noreply.github.com>
2023-02-14 17:21:18 +0530

    Update ec2-creation.tf

--- terraformscripts/ec2-creation.tf
+++ terraformscripts/ec2-creation.tf
@@ -2,7 +2,7 @@ provider "aws" {
 	region = "us-east-1"
 } 
 resource "aws_instance" "AwsInstance"{
-	ami = "ami-0aa7d40eeae50c9a9"
+	ami = "ami-0c9978668f8d55984"
 	instance_type = "t2.micro"
 	key_name = "Myserver1"
 	security_groups = ["launch-wizard-44"]
